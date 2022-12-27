387706ed7fe62b6a041775cadcada44fbf9a7d51
André Cabral <63832522+AndreLucas2103@users.noreply.github.com>
2022-12-27 23:45:40 +0000

    1

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3030);
+  await app.listen(process.env.PORT || 3030, () => {
+    console.log('listening on port ' + process.env.PORT || '3030');
+  });
 }
 bootstrap();
